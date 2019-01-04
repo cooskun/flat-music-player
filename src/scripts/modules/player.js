22cba49f61251e8ba5d329d96aac86399c07bf1d
coskunbaris <coskun.bris@gmail.com>
2019-01-04 10:37:10 +0100

    Refactor the player before pushing to Codepen

--- src/scripts/modules/player.js
+++ src/scripts/modules/player.js
@@ -1,150 +1,161 @@
-import { Howl } from "../../../node_modules/howler/dist/howler";
+import { Howl } from 'howler'
 
 // Element selectors
-const elPlay = document.querySelector(".js-play");
-const elBackward = document.querySelector(".js-backward");
-const elForward = document.querySelector(".js-forward");
-const elPlayer = document.querySelector(".js-player");
-const elProgressbar = document.querySelector(".js-progress");
+const elPlay = document.querySelector('.js-play')
+const elBackward = document.querySelector('.js-backward')
+const elForward = document.querySelector('.js-forward')
+const elPlayer = document.querySelector('.js-player')
+const elProgressbar = document.querySelector('.js-progress')
+const elLoading = document.querySelector('.js-loading')
+const elSCene = document.querySelector('.scene')
 
-let progress = 0; // int
-let duration;
-let ratio;
-let soundStatu = 0; // int
-let lastAction;
+let progress = 0 // int
+let duration
+let ratio
+let soundStatus = 0 // int
+let lastAction
 
 const sound = new Howl({
-  src: "sound/song.mp3",
+  src: 'sound/song.mp3',
 
   onload: () => {
-    // Sound Loaded
+    // Remove the Loading
+    removeLoading()
 
     // Fading sound for intro
-    sound.fade(0, 1, 1500, sound.play());
+    sound.fade(0, 1, 1500, sound.play())
 
-    // Gets sound total time
-    duration = Math.round(sound.duration());
+    // Gets total time sound
+    duration = Math.round(sound.duration())
 
     // Gets the ratio and base it to 1000 for setInterval
-    ratio = duration / 100;
+    ratio = duration / 100
 
-    setInterval(fireProgressbar, 1000);
+    setInterval(fireProgressbar, 1000)
   },
 
   onplay: () => {
     // Sound has been played, state class is added for animations
-    elPlayer.classList.add("is-playing");
+    elPlayer.classList.add('is-playing')
 
-    setIconToPlay("fa-pause");
+    setIconToPlay('fa-pause')
 
     // Make aware that sound is started
-    lastAction = "play";
+    lastAction = 'play'
   },
 
   onpause: () => {
     // Sound has been paused, stops animations
-    elPlayer.classList.remove("is-playing");
+    elPlayer.classList.remove('is-playing')
 
-    setIconToPlay("fa-play");
+    setIconToPlay('fa-play')
 
     // Make aware that sound is paused
-    lastAction = "pause";
+    lastAction = 'pause'
   },
 
   onend: () => {
     // Stop the player, sound is over
-    elPlayer.classList.remove("is-playing");
+    elPlayer.classList.remove('is-playing')
 
-    setIconToPlay("fa-spinner fa-spin");
+    setIconToPlay('fa-spinner fa-spin')
 
     // Restart the sound after 5 seconds
-    setTimeout(restart, 5000);
+    setTimeout(restart, 5000)
   }
-});
+})
+
+const removeLoading = () => {
+  elLoading.classList.add('is-loaded')
+
+  elLoading.addEventListener('transitionend', () => {
+    elLoading.remove()
+  })
+}
 
 const playOrPause = () => {
-  sound.playing() ? sound.pause() : sound.play();
-};
+  sound.playing() ? sound.pause() : sound.play()
+}
 
 const setIconToPlay = iconName => {
-  const icon = `<i class="fa ${iconName}"></i>`;
+  const icon = `<i class="fa ${iconName}"></i>`
 
-  elPlay.innerHTML = icon;
-};
+  elPlay.innerHTML = icon
+}
 
 const progressController = () => {
-  if (soundStatu < 0 || progress < 0) {
-    soundStatu = 0;
-    progress = 0;
+  if (soundStatus < 0 || progress < 0) {
+    soundStatus = 0
+    progress = 0
   }
-};
+}
 
 const setToProgressbar = () => {
-  elProgressbar.style.width = `${progress}%`;
-};
+  elProgressbar.style.width = `${progress}%`
+}
 
 const seekBack = time => {
-  lastAction = "seekBack";
-  progress = progress - time / ratio;
-  soundStatu = soundStatu - time;
+  lastAction = 'seekBack'
+  progress = progress - time / ratio
+  soundStatus = soundStatus - time
 
-  progressController();
+  progressController()
 
-  sound.seek(soundStatu);
+  sound.seek(soundStatus)
 
-  setToProgressbar();
-};
+  setToProgressbar()
+}
 
 const seekForward = time => {
-  lastAction = "seekForward";
-  progress = progress + time / ratio;
-  soundStatu = soundStatu + time;
+  lastAction = 'seekForward'
+  progress = progress + time / ratio
+  soundStatus = soundStatus + time
 
-  progressController();
+  progressController()
 
-  sound.seek(soundStatu);
+  sound.seek(soundStatus)
 
-  setToProgressbar();
-};
+  setToProgressbar()
+}
 
 const restart = () => {
-  soundStatu = 0;
-  progress = 0;
-  sound.seek(0);
+  soundStatus = 0
+  progress = 0
+  sound.seek(0)
 
-  setToProgressbar();
+  setToProgressbar()
 
-  sound.play();
-};
+  sound.play()
+}
 
 function fireProgressbar() {
   // Sound is paused, don't do anything
-  if (lastAction == "pause") {
-    return;
+  if (lastAction == 'pause') {
+    return
   }
 
   // Sound is playing, set it to progress
-  else if (lastAction == "play") {
-    progress = progress + 1 / ratio;
-    soundStatu++;
-    elProgressbar.style.width = `${progress}%`;
+  else if (lastAction == 'play') {
+    progress = progress + 1 / ratio
+    soundStatus++
+    elProgressbar.style.width = `${progress}%`
   }
 
   // Action statu is not play or pause, then turn it to play for move on
   else {
-    lastAction = "play";
+    lastAction = 'play'
   }
 }
 
 // Plays/pauses the sound
-elPlay.addEventListener("click", playOrPause);
+elPlay.addEventListener('click', playOrPause)
 
 // Go back 5 seconds in sound
-elBackward.addEventListener("click", () => {
-  seekBack(5);
-});
+elBackward.addEventListener('click', () => {
+  seekBack(5)
+})
 
 // Go forward 5 seconds in soun
-elForward.addEventListener("click", () => {
-  seekForward(5);
-});
+elForward.addEventListener('click', () => {
+  seekForward(5)
+})
